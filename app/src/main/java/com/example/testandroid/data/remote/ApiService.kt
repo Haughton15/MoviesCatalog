b109ba4a3c488cb3a66b4e48fc6fd16409221fde
Haughton15 <omarbenhau15@gmail.com>
2023-03-03 16:08:32 -0500

    Proyect Initial commit

---
+++ app/src/main/java/com/example/testandroid/data/remote/ApiService.kt
@@ -0,0 +1,16 @@
+package com.example.testandroid.data.remote
+
+import com.example.testandroid.data.model.GetMoviesResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("movie/popular")
+    suspend fun getPopularMovies(@Query("api_key") apiKey: String,
+                                 @Query("genre") genre: String): Response<GetMoviesResponse>
+
+    @GET("movie/top_rated")
+    suspend fun getTopRatedMovies(@Query("api_key") apiKey: String ): Response<GetMoviesResponse>
+}
